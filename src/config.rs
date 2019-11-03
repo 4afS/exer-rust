d7736f6ac3a703b07cf87120ea1b72622631dd86
4afS <g2x649@gmail.com>
2019-11-04 08:06:24 +0900

    add comment for configs

--- src/config.rs
+++ src/config.rs
@@ -2,13 +2,28 @@ use super::types::Config;
 
 pub fn generate_config() -> Vec<Config> {
     vec![
+        // Rust
         Config::new(vec!["Cargo.toml", "Cargo.lock"], "cargo run"),
-        Config::new(vec!["build.sbt"], "sbt run"),
+
+        // Haskell
         Config::new(vec!["stack.yaml"], "stack run"),
+
+        // PureScript
         Config::new(vec!["spago.dhall", ".spago"], "spago run"),
+
+        // Elm
         Config::new(vec!["elm.json"], "elm reactor"),
+
+        // Java and Kotlin
         Config::new(vec!["build.gradle"], "gradle run"),
+
+        // Scala
+        Config::new(vec!["build.sbt"], "sbt run"),
+
+        // Golang
         Config::new(vec!["go.mod"], "go run"),
+        
+        // Dlang
         Config::new(vec!["dub.json", "dub.sdl"], "dub run"),
     ]
 }
