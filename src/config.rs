71d637eb1c92a59231ecbf88e59e2f35a4f5468f
4afS <g2x649@gmail.com>
2019-11-04 07:52:25 +0900

    support Dlang

--- src/config.rs
+++ src/config.rs
@@ -4,10 +4,11 @@ pub fn generate_config() -> Vec<Config> {
     vec![
         Config::new(vec!["Cargo.toml", "Cargo.lock"], "cargo run"),
         Config::new(vec!["build.sbt"], "sbt run"),
-        Config::new(vec!["stack.yaml", "package.yaml"], "stack run"),
+        Config::new(vec!["stack.yaml"], "stack run"),
         Config::new(vec!["spago.dhall", ".spago"], "spago run"),
         Config::new(vec!["elm.json"], "elm reactor"),
         Config::new(vec!["build.gradle"], "gradle run"),
         Config::new(vec!["go.mod"], "go run"),
+        Config::new(vec!["dub.json", "dub.sdl"], "dub run"),
     ]
 }
